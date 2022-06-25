6cb8e6f1fd9071b5101a241f44f528b5b99e60ef
otrachea <xtrachea@gmail.com>
2022-06-25 14:13:24 -0400

    Created basic test query but front to back all works

--- src/server.js
+++ src/server.js
@@ -1,13 +1,32 @@
 import express from "express";
-import { MongoClient } from "mongodb";
-
+import expressSession from "express-session";
 import { clientPromise } from "./db/db.js";
+
+import { getAllCards } from "./db/queries/01_cards.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("test");
 });
+
+app.get("/cards", (req, res) => {
+  getAllCards().then((data) => {
+    console.log(data);
+    res.json({ PLS: "WORK" });
+  });
+});
+
+app.use(
+  expressSession({
+    secret: "fkdlsfjkldsfjldskfjds",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 15 * 60 * 1000,
+    },
+  })
+);
 
 app.listen(3001, () => {
   console.log(`Server listening on port ${3001}`);
