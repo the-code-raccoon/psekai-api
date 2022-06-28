4a847dbde7378dd0c12937c10a938988fce8b22c
otrachea <xtrachea@gmail.com>
2022-06-27 21:38:44 -0400

    /cards now returns all cards in db as json

--- src/server.js
+++ src/server.js
@@ -12,9 +12,12 @@ app.get("/", (req, res) => {
 
 app.get("/cards", async (req, res) => {
   const cards = await getAllCards();
-
+  const json = {}
+  for (const card of cards) {
+    json[card._id] = card;
+  }
   console.log(cards);
-  res.json({ PLS: "WORK" });
+  res.json(json);
 });
 
 app.get("/cards2", async (req, res) => {
